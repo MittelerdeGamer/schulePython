218d07f0ea80c25be613c7fa52c017c5fe19d246
Jannis <jannis.swientek@web.de>
2022-11-02 11:46:57 +0100

    added LF05_S560.py

---
+++ LF05_S560.py
@@ -0,0 +1,72 @@
+import random
+
+
+def aufg2():
+    anzahl = 6
+    i = 0
+    summe = 0
+    temperaturen = []  # Anlegen einer leeren Liste
+    while i < anzahl:
+        print("Geben Sie die {0}. Temperatur in ° C ein".format(i + 1))
+        eingabe = float(input())
+        temperaturen.append(eingabe)  # Element am Ende der Liste hinzufügen
+        summe = summe + temperaturen[i]  # auf Listenelement zugreifen
+        i = i + 1
+    durchschnitt = summe / anzahl
+
+    print("Durchschnitt: " + str(durchschnitt) + "°C")
+
+    for j in range(0, anzahl):
+        print(str(temperaturen[j]) + "°C am Tag " + str(j + 1))
+
+
+def aufg3():
+    anzahl = 31
+    i = 0
+    summe = 0
+    temperaturen = []  # Anlegen einer leeren Liste
+    rand = random
+    rand.seed()
+
+    while i < anzahl:
+        eingabe = float(rand.randint(0, 25))
+        temperaturen.append(eingabe)  # Element am Ende der Liste hinzufügen
+        summe = summe + temperaturen[i]  # auf Listenelement zugreifen
+        i = i + 1
+    durchschnitt = summe / anzahl
+
+    print("Durchschnitt: " + str(durchschnitt) + "°C")
+
+    for j in range(0, anzahl):
+        print(str(temperaturen[j]) + "°C am Tag " + str(j + 1))
+
+
+def aufg4():
+    for i in range(0, 20):
+        if i % 2 == 0:
+            print(i)
+
+
+def aufg5():
+    anzahl = 8
+
+    odd = 0
+    if anzahl % 2 == 0:
+        odd = anzahl + 1
+    else:
+        odd = anzahl
+
+    for i in range(anzahl, 0, -1):
+
+        if i % 2 == 0:
+            print()
+        else:
+            s = "\t"
+            offset = int((odd - i) / 2)
+            for j in range(0, offset):
+                s = s + " "
+            for j in range(0, i):
+                s = s + "*"
+            for j in range(0, offset):
+                s = s + " "
+            print(s + "\t" + str(i) + " Sterne")
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from tools import *
-from LF05_04_05 import *
+from LF05_S560 import *
 
 if __name__ == '__main__':
     print('PyCharm')
-    #aufg1()
-    #aufg2()
-    #aufg3()
-    hex2bin()
+    aufg5()
+
+
 
 
 
